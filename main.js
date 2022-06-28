c4065e11a0c0f5a3816b3dc2d56702aeca3b6c26
Vitor Gomes <88215288+odevz@users.noreply.github.com>
2022-06-27 23:40:28 -0300

    final

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    // topo da sessão
+    const sectionTop = section.offsetTop
+    // altura da sessão
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    const sectionBundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBundaries) {
+        menuElement.classList.add('active')
+    }
+}
+
+function showNavOnScroll() {
     if (scrollY > 0) {
         document.getElementById('navigation').classList.add('scroll')
     } else {
         document.getElementById('navigation').classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 400) {
+        document.getElementById('backToTopButton').classList.add('show')
+    } else {
+        document.getElementById('backToTopButton').classList.remove('show')
     }
 }
 
@@ -13,3 +57,18 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card
+    #about,
+    #about header,
+    #about .content`);
